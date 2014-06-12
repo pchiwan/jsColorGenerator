3ea41f4211530f28c5a4b8710c3e777ed015b7b7
pchiwan <silvia.murblanch@gmail.com>
2014-06-12 17:05:09 +0200

    Update page and demo

--- javascripts/demo.js
+++ javascripts/demo.js
@@ -2,6 +2,40 @@
 
 $(document).ready(function () {
 
-	//TODO
+	var viewModel = function () {
+
+		var self = this;
+
+		var colGen = new jsColorGenerator({
+			//avoidDarks: true,
+			//avoidLights: true
+		});
+
+		this.colors1 = ko.observableArray([]);
+		this.colors2 = ko.observableArray([]);
+		this.colors3 = ko.observableArray([]);
+		this.colorsDemo = ko.observableArray([]);	
+
+		function refreshColors () {
+			var temp = [];
+			for (var i = 0; i < 12; i++) {
+				temp.push(colGen.getColor());
+			}
+			return temp;
+		}
+
+		this.refreshAll = function () {
+			self.colors1(refreshColors());
+			self.colors2(refreshColors());
+			self.colors3(refreshColors());
+		};
+
+		return this;
+	}();
+
+	viewModel.refreshAll();
+
+	//apply ko bindings
+	ko.applyBindings(viewModel, $('body')[0]);
 
 });
